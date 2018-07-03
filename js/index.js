d5fdea4a090e6efaea474efb079e445c5d5a542e
326416055@qq.com <326416055@qq.com>
2018-07-03 11:49:51 +0800

    q

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function () {
+    "use strict";
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+})
+
+
+
+
